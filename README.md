9bb9da35064d10bc0981152c634824ec99ee6598
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2020-10-23 17:34:25 -0300

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# node-templates
+# Ejemplo Mercado Pago Node
+
+
+
+
+
+
+
+## Configura tu aplicación
+
+
+Remplazar en los archivos de la carpeta config, los valores 77778888 y xxx%xxx por lo correspondiente.
+
+
+## Configuración frontend
+
+Para proceder al checkout, debes enviar un formulario con el detalle de los items como figura en 
+
+
+
+### Utilidades externas
+
+[node-template](https://github.com/arielivandiaz/node-templates "node-template")
+
+[Lagunite Frontned Framework](http://lagunite.com/ "Lagunite Frontned Framework ")
+
+[Auto Commit Script](https://github.com/arielivandiaz/auto-commit "Auto Commit Script")
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+const mercadopago = require("mercadopago");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const payer = require('../config/mp.user.test.json');
+const default_preference = require('../config/mp.conf.json');
+const token = require('../config/mp.token.json');
+
+mercadopago.configure({
+  access_token: token.access_token,
+  integrator_id: token.integrator_id
 });
+
+
+/* POST A FORM WITH USER DETAILS LIKE : ../config/mp.user.test.json */
+router.post('/checkout', function (req, res) {
+
+  let preference = default_preference;
+
+  preference.payer = payer;
+  preference.items = [req.body];
+  
+  // forEach item do : 
+  preference.items[0].quantity =  parseInt(req.body.quantity);
+  preference.items[0].unit_price =  parseFloat(req.body.unit_price)
+
+  mercadopago.preferences.create(preference)
+    .then( (response) => {
+      var url = "https://www.mercadopago.com/mla/checkout/start?pref_id=" + response.body.id;      
+      res.redirect(url);
+    }).catch(function (error) {
+      console.log(error);
+    });
+
+});
+
+
+/* IPN Handler */
+router.get('/ipn', function (req, res) {
+  console.log("IPN GET");
+  console.log(req.query);
+  console.log("/*IPN GET");
+});
+
+router.post('/ipn', function (req, res) {
+
+  console.log("IPN POST");
+  console.log(req.body);
+  console.log("/*IPN POST");
+});
+
+
+
 
 module.exports = router;
